f979d6d07d35f1471aefc17b2d54b705363f88f9
nidazeeshan123 <127075788+nidazeeshan123@users.noreply.github.com>
2023-03-07 16:44:29 +0500

    upd

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,27 @@
+﻿using JWTToken.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticate with Jwt";
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string AddUser(Users user)
+        {
+            return "user added" + user.Username;
+        }
+    }
+}
